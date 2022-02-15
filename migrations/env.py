ab212a8e14735addc6513e53cd2b7bda32cc5703
noviicee_U <cseanamika3@gmail.com>
2022-02-15 15:38:30 +0530

    fixed a flake8 error

--- migrations/env.py
+++ migrations/env.py
@@ -2,7 +2,7 @@ from __future__ import with_statement
 
 import logging
 from logging.config import fileConfig
-from typing import List, Any
+from typing import Any, List
 
 from alembic import context
 from flask import current_app
